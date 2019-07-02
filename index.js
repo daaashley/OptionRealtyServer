78d87c780f5d3f5533e3db4570ba265d377767da
David Ashley <david@davids-mbp.lan>
2019-07-01 20:53:05 -0500

    init and docker

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  console.log("home");
+  res.send("hello");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log("App is running on port " + port);
